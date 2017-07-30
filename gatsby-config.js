1eca0b1220f8121992c6f1b7a74adad95eaa68ef
Tom Hastjarjanto <tom@intellicode.nl>
2017-07-30 22:21:45 +0200

    init: Configure additional plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,29 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/posts`,
+        name: 'posts',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: 'language-',
+            },
+          },
+        ]
+      }
+    },
+  ],
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './index.css'
+import './index.scss'
 
-const Header = () => (
+const Header = () =>
   <div
     style={{
       background: 'rebeccapurple',
@@ -28,36 +28,32 @@ const Header = () => (
           }}
         >
           Gatsby
-            </Link>
+        </Link>
       </h1>
     </div>
   </div>
-)
 
-const TemplateWrapper = ({
-  children
-}) => (
-    <div>
-      <Helmet
-        title="Gatsby Default Starter"
-        meta={[
-          { name: 'description', content: 'Sample' },
-          { name: 'keywords', content: 'sample, something' },
-        ]}
-      />
-      <Header />
-      <div
-        style={{
-          margin: '0 auto',
-          maxWidth: 960,
-          padding: '0px 1.0875rem 1.45rem',
-          paddingTop: 0,
-        }}
-      >
-        {children()}
-      </div>
+const TemplateWrapper = ({ children }) =>
+  <div>
+    <Helmet
+      title="Gatsby Default Starter"
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+      ]}
+    />
+    <Header />
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0px 1.0875rem 1.45rem',
+        paddingTop: 0,
+      }}
+    >
+      {children()}
     </div>
-  )
+  </div>
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
