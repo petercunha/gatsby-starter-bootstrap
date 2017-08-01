37ad56b3601b4429e948043f2da9bbee6841cfa1
Tom Hastjarjanto <tom@intellicode.nl>
2017-08-01 23:50:30 +0200

    init: Add bootstrap blog template

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -13,8 +13,81 @@ const TemplateWrapper = ({ children }) =>
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
       ]}
+      link={[
+        { 
+          rel: 'stylesheet',
+          href: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css',
+          integrity: 'sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ',
+          crossorigin: 'anonymous',
+        }
+      ]} 
     />
-    {children()}
+
+    <div className="blog-masthead">
+      <div className="container">
+        <nav className="nav blog-nav">
+          <a className="nav-link active" href="#">Home</a>
+          <a className="nav-link" href="#">New features</a>
+          <a className="nav-link" href="#">Press</a>
+          <a className="nav-link" href="#">New hires</a>
+          <a className="nav-link" href="#">About</a>
+        </nav>
+      </div>
+    </div>
+
+    <div className="blog-header">
+      <div className="container">
+        <h1 className="blog-title">The Bootstrap Blog</h1>
+        <p className="lead blog-description">An example blog template built with Bootstrap.</p>
+      </div>
+    </div>
+
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-8 blog-main">
+          {children()}
+
+        </div>
+
+        <div className="col-sm-3 offset-sm-1 blog-sidebar">
+          <div className="sidebar-module sidebar-module-inset">
+            <h4>About</h4>
+            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+          </div>
+          <div className="sidebar-module">
+            <h4>Archives</h4>
+            <ol className="list-unstyled">
+              <li><a href="#">March 2014</a></li>
+              <li><a href="#">February 2014</a></li>
+              <li><a href="#">January 2014</a></li>
+              <li><a href="#">December 2013</a></li>
+              <li><a href="#">November 2013</a></li>
+              <li><a href="#">October 2013</a></li>
+              <li><a href="#">September 2013</a></li>
+              <li><a href="#">August 2013</a></li>
+              <li><a href="#">July 2013</a></li>
+              <li><a href="#">June 2013</a></li>
+              <li><a href="#">May 2013</a></li>
+              <li><a href="#">April 2013</a></li>
+            </ol>
+          </div>
+          <div className="sidebar-module">
+            <h4>Elsewhere</h4>
+            <ol className="list-unstyled">
+              <li><a href="#">GitHub</a></li>
+              <li><a href="#">Twitter</a></li>
+              <li><a href="#">Facebook</a></li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer className="blog-footer">
+      <p>Blog template built for <a href="https://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
   </div>
 
 TemplateWrapper.propTypes = {
