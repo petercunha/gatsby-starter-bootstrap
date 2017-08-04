ede8e71860e76789453ef91c93bd39949fd492f0
Tom Hastjarjanto <tom@intellicode.nl>
2017-08-04 22:56:42 +0200

    feature/add-archive-pages: Add archive list component

---
+++ src/templates/archiveList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+export default function ArchiveList({
+  data
+}) {
+  const { edges: posts } = data.archive;
+  const months = posts
+    .reduce((months, { node }) => {
+      months[node.frontmatter.path] = node.frontmatter.title;
+      return months;
+    }, {});
+
+  return (
+    <ol className="list-unstyled">
+        {Object.keys(months).map((key) => {
+            const title = months[key];
+            return (
+                <li key={key}>
+                    <Link to={`${key}/`}>{title}</Link>
+                </li>
+            );
+            })}
+    </ol>
+  );
+}
