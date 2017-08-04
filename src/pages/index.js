c57c8543b76004353d4eac5b2d5e09fafd8052b9
Tom Hastjarjanto <tom@intellicode.nl>
2017-08-04 22:57:30 +0200

    feature/add-archive-pages: Move body logic to page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import List from '../templates/list';
+import ArchiveList from '../templates/archiveList';
+import Link from 'gatsby-link';
 
 export default function Index({
   data
 }) {
-  return <List data={data} />;
+  return (
+    <div className="row">
+      <div className="col-sm-8 blog-main">
+          <List data={data} />
+      </div>
+      <div className="col-sm-3 offset-sm-1 blog-sidebar">
+        <div className="sidebar-module sidebar-module-inset">
+          <h4>About</h4>
+          <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+        </div>
+        <div className="sidebar-module">
+          <h4>Archives</h4>
+          <ArchiveList data={data} />
+        </div>
+        <div className="sidebar-module">
+          <h4>Elsewhere</h4>
+          <ol className="list-unstyled">
+            <li><a href="#">GitHub</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Facebook</a></li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export const pageQuery = graphql`
@@ -18,6 +44,16 @@ export const pageQuery = graphql`
             title
             date(formatString: "MMMM DD, YYYY")
             path
+          }
+        }
+      }
+    }
+    archive: allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            path: date(formatString: "Y-MM")
+            title: date(formatString: "MMMM Y")
           }
         }
       }
